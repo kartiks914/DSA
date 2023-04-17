62760f07317e62d096fdfa3a7a2ad812fbc9fc0c
kartiks914 <67848606+kartiks914@users.noreply.github.com>
2023-04-17 22:36:54 +0530

    Time: 4 ms (34.30%), Space: 42.5 MB (32.24%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<sentences.length;i++){
+            int count =0;
+             for(int j=0;j<sentences[i].length();j++)
+             {
+                
+                if(sentences[i].charAt(j)==' ')
+                {
+                    count++;
+                }
+            }
+        if(max < count+1){
+            max= count+1;
+        }
+        }
+
+        return max;
+    }
+}
